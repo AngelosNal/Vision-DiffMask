508aa601930c7154e92e3138fdd9c03ea7345e0e
AngelosNal <angelosnal97@gmail.com>
2022-05-06 16:45:06 +0200

    Fixed bug.

--- code/lightningmodule.py
+++ code/lightningmodule.py
@@ -6,20 +6,22 @@ import pytorch_lightning as pl
 class ImageClassificationNet(pl.LightningModule):
     def __init__(self, model, lr):
         super().__init__()
-        self.save_hyperparameters()
+
+        self.lr = lr
+
         self.model = model
 
     def forward(self, x):
-        return self.model(x)
+        return self.model(x).logits
 
     def configure_optimizers(self):
-        optimizer = optim.AdamW(self.parameters(), lr=self.hparams.lr)
-        lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100,150], gamma=0.1)
+        optimizer = optim.AdamW(self.parameters(), lr=self.lr)
+        lr_scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150], gamma=0.1)
         return [optimizer], [lr_scheduler]
 
     def _calculate_loss(self, batch, mode="train"):
         imgs, labels = batch
-        preds = self.model(imgs)
+        preds = self.model(imgs).logits
         loss = F.cross_entropy(preds, labels)
         acc = (preds.argmax(dim=-1) == labels).float().mean()
 
